ceaba71f245e9ae3a9d8034a98c8bf05b29445e0
SJayasinghe <it19126234@my.sliit.lk>
2021-08-16 06:45:36 -0700

    Create teacher adding form AB#36

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Layout from './Layout/Layout';
 import SubjectsPanel from './Subjects/SubjectsPanel';
 import AddSubject from './Subjects/AddSubject';
 import './App.css';
+import Teachers from './Teachers/Teachers';
+import AddTeacher from './Teachers/AddTeacher';
 
 function App() {
   return (
@@ -20,6 +22,9 @@ function App() {
 
           <Route path='/Subjects' exact component={SubjectsPanel} />
           <Route path='/Subjects/Add' exact component={AddSubject} />
+
+          <Route path='/Teachers' exact component={Teachers} />
+          <Route path='/Teachers/Add' exact component={AddTeacher} />
 
         </Switch>
       </Layout>
