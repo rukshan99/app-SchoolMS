71b53a8b20145246967066153f07510f9c0df621
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-08-18 19:16:02 +0530

    add api endpoints

--- src/Students/StudentsTable.js
+++ src/Students/StudentsTable.js
@@ -38,7 +38,7 @@ class StudentsTable extends Component {
 	};
 
 	goToDetails = studentId => {
-		this.props.history.push(`/Students/Details/${studentId}`);
+		this.props.history.push(`/students/${studentId}`);
 	};
 	delete = async (studentId, studentName) => {
 		if (window.confirm(`Are you sure you want to delete a teacher with name ${studentName} ?`)) {
