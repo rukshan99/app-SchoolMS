3cfc3551cc57d18ea58701db5986592f8d15d8c2
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-09-06 21:25:50 +0530

    update graph colors to generate randomly

--- src/Students/StudentsByAge.js
+++ src/Students/StudentsByAge.js
@@ -10,22 +10,8 @@ class StudentsByAge extends Component {
         {
           label: '# of Students',
           data: [],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
+          backgroundColor: [],
+          borderColor: [],
           borderWidth: 1,
         },
       ],
@@ -83,7 +69,7 @@ class StudentsByAge extends Component {
       const color2 = Math.floor(Math.random() * 256);
       const color3 = Math.floor(Math.random() * 256);
       backgroundColors.push(`rgba(${color1}, ${color2}, ${color3}, 0.2)`);
-      borderColors.push(`rgba(${color1}, ${color2}, ${color3}, 0.1)`);
+      borderColors.push(`rgba(${color1}, ${color2}, ${color3}, 1)`);
     }
     this.setState({
       data: {
